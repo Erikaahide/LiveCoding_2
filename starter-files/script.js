4c9b7c6e0d826c848f8b92575ea7f217f592bb03
Erikaahide <eahidehernandez@gmail.com>
2025-09-15 13:47:24 -0600

    bonus

--- starter-files/script.js
+++ starter-files/script.js
@@ -4,6 +4,7 @@
  * 2. Output the API's response
  * 3. Attach fetchAnswer to an event listener
  * 4. Clear output after 5 seconds
+ * 5. Validate cannot click button if the input is empty.
  */
 
 
@@ -12,6 +13,17 @@ const input  = document.getElementById('input');
 const answer = document.getElementById('answer');
 const btn      = document.getElementById('button');
 let clearTimer;
+let loading = false;
+
+//funcion para validar texto al clickear boton
+function updateButton() {
+  const hasText = input.value.trim().length > 0;
+  btn.disabled = !hasText || loading;
+}
+
+// Inicializa estado del botón y actualiza al escribir
+updateButton();
+input.addEventListener('input', updateButton);
 
 //funcion para limpiar despues de 5 segundos
 function clearFields() {
@@ -25,14 +37,19 @@ function fetchAnswer() {
         if (!response.ok){ //valida el request 
         throw new Error ('Error de conexion') //manejo de errores 
         } return response.json();//parsear fectch a json
+        
     })
     .then(data => {
       answer.textContent = String(data.answer).toUpperCase();
+      loading = true;
+      updateButton();
       clearTimer = setTimeout(clearFields, 5000);
     })
     .catch(err => {
       console.error(err);
       answer.textContent = 'Ocurrió un error';
+      loading = false;
+      updateButton();
       clearTimer = setTimeout(clearFields, 5000);
     });
 }
